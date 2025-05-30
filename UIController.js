d6e93e9c7307235fdc51914a48ce3260ff4b63cf
Maxim <195960242+LizardVizard@users.noreply.github.com>
2025-05-30 23:15:49 +0300

    feat(UIController): implement UIController for rendering boards
    Add a UIController class responsible for rendering a Gameboard to a provided DOM element.
    
    Note: Tests are not implemented.

---
+++ UIController.js
@@ -0,0 +1,93 @@
+import { EMPTY, MISS } from "./Gameboard.js";
+
+export class UIController {
+  constructor(board1, board2) {
+    this.cells = [];
+
+    this.boardContainer1 = board1;
+    this.boardContainer2 = board2;
+  }
+
+  renderBoard(board, boardContainer, isCurrentPlayer, showShips = false) {
+    boardContainer.innerHTML = "";
+    const boardSize = board.size;
+    this.cells = [];
+
+    if (!isCurrentPlayer) {
+      boardContainer.classList.add("activated");
+      boardContainer.classList.remove("deactivated");
+    } else {
+      boardContainer.classList.remove("activated");
+      boardContainer.classList.add("deactivated");
+    }
+
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.dataset.x = i;
+        cell.dataset.y = j;
+
+        const boardCellValue = board.cells[i][j];
+        switch (boardCellValue) {
+          case EMPTY:
+            break;
+          case MISS:
+            cell.classList.add("miss");
+            cell.innerText = "•";
+            break;
+          default:
+            const ship = board.shipEntries[boardCellValue].ship;
+
+            if (ship.hits.find((hit) => hit[0] === i && hit[1] === j)) {
+              if (ship.isSunk()) cell.classList.add("hit");
+              cell.innerText = "❌";
+            }
+            if (!isCurrentPlayer && showShips) cell.classList.add("ship");
+            break;
+        }
+
+        if (!isCurrentPlayer) this.attachOnClickToCell(cell);
+        boardContainer.appendChild(cell);
+      }
+    }
+  }
+
+  render(player1, player2, currentPlayer, showShips = false) {
+    this.renderBoard(
+      player1.board,
+      this.boardContainer1,
+      player1 === currentPlayer,
+      showShips,
+    );
+    this.renderBoard(
+      player2.board,
+      this.boardContainer2,
+      player2 === currentPlayer,
+      showShips,
+    );
+  }
+
+  attachOnClickToCell(cell) {
+    const x = Number(cell.dataset.x);
+    const y = Number(cell.dataset.y);
+    cell.addEventListener("mousedown", (e) => {
+      if (this.cellClickHandler) this.cellClickHandler(e, x, y);
+    });
+  }
+
+  onCellClick(callback) {
+    this.cellClickHandler = callback;
+  }
+
+  // TODO: Maybe add ship placement visualisation
+  attachOnHoverToCell(cell) {
+    cell.addEventListener("hover", (e) => {
+      if (this.cellHoverHandler) this.cellHoverHandler(e, cell);
+    });
+  }
+
+  onCellHover(callback) {
+    this.cellHoverHandler = callback;
+  }
+}
---
+++ UIController.test.js
@@ -0,0 +1,5 @@
+describe("UI controller", () => {
+  describe("boards creation", () => {
+    it.todo("should create a board with appropriate size");
+  });
+});
